e2410016485d7591572ffced9d451fc62cb449a6
Adrian Subitz <adrian.subitz@iquestgroup.com>
2019-10-22 12:58:45 +0300

    Added solution

---
+++ iQuestUniversity/Program.cs
@@ -0,0 +1,25 @@
+﻿using Animal;
+using System;
+
+namespace iQuestUniversity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"iQuest University debugging{Environment.NewLine}");
+
+            Duck duck = new Duck();
+            duck.Sound();
+
+            Dog dog = new Dog();
+            dog.Sound();
+
+            Cat cat = new Cat();
+            cat.Sound();
+
+            Console.WriteLine($"{Environment.NewLine}Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
