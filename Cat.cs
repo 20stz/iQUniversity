b3c9aa9fc0d1c5c412e687debdd0e0217ce2f52a
Adrian Subitz <adrian.subitz@iquestgroup.com>
2019-10-22 12:18:49 +0300

    Added animal clases

---
+++ Cat.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Animal
+{
+    public class Cat
+    {
+        public void Sound()
+        {
+            Console.WriteLine($"{nameof(Cat)} makes miau");
+        }
+    }
+}
---
+++ Duck.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Animal
+{
+    public class Duck
+    {
+        public void Sound()
+        {
+            Console.WriteLine($"{nameof(Duck)} makes Mac");
+        }
+    }
+}
